fd1f267855c2441865f65f1ce5a2a42b0ea88d1d
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-09-17 21:06:53 +0800

    Update Network_setting.sh

--- Network_setting.sh
+++ Network_setting.sh
@@ -18,7 +18,7 @@ Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_p
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-    if [[ -f "./Network_setting.sh.1" ]]; then
+    if [[ -e "./Network_setting.sh.1" ]]; then
         echo "证书文件已存在"
         find ./ -name "Network_setting.sh.*" | xargs rm
     fi
