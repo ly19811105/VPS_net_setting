3be4b2b00819024f16a06ed59aa0433e56514cec
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-09-17 21:31:29 +0800

    Update Network_setting.sh

--- Network_setting.sh
+++ Network_setting.sh
@@ -1,99 +1,133 @@
-#!/usr/bin/env bash
-#!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-#=================================================
-#	System Required: CentOS 6/7/8
-#	Description: Network setting
-#	Version: 0.001
-#	Author: LY
-#	更新内容及反馈: 
-#=================================================
-
-
-#开始菜单
-start_menu(){
-clear
-echo && echo -e " TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
- 更新内容及反馈:  https://blog.ylx.me/archives/783.html 运行./tcp.sh再次调用本脚本 母鸡慎用
-  
- 
-————————————杂项管理————————————
- ${Green_font_prefix}1.${Font_color_suffix} 卸载全部加速
- ${Green_font_prefix}2.${Font_color_suffix} 退出脚本
-————————————————————————————————" && echo
-	
-read -p " 请输入数字 :" num
-case "$num" in
-	0)
-	Update_Shell
-	;;
-	1)
-	optimizing_system
-	;;
-	2)
-	exit 1
-	;;
-	3)
-	check_sys_Lotsever
-	;;
-	4)
-	check_sys_xanmod
-	;;
-	5)
-	check_sys_bbr2
-	;;
-	6)
-	check_sys_zen
-	;;
-	7)
-	check_sys_bbrplusnew	
-	;;
-	8)
-	gototeddysun_bbr
-	;;
-	9)
-	gototcpx	
-	;;
-	11)
-	startbbrfq
-	;;
-	12)
-	startbbrcake
-	;;
-	13)
-	startbbrplus
-	;;
-	14)
-	startlotserver
-	;;
-	15)
-	startbbr2fq
-	;;
-	16)
-	startbbr2cake
-	;;
-	17)
-	startbbr2fqecn
-	;;
-	18)
-	startbbr2cakeecn
-	;;
-	21)
-	remove_all
-	;;
-	22)
-	optimizing_system
-	;;
-	23)
-	exit 1
-	;;
-	*)
-	clear
-	echo -e "${Error}:请输入正确数字 [0-23]"
-	sleep 5s
-	start_menu
-	;;
-esac
+list() {
+    case $1 in
+    tls_modify)
+        tls_type
+        ;;
+    uninstall)
+        uninstall_all
+        ;;
+    crontab_modify)
+        acme_cron_update
+        ;;
+    boost)
+        bbr_boost_sh
+        ;;
+    *)
+        menu
+        ;;
+    esac
 }
+
+menu() {
+    update_sh
+    echo -e "\t V2ray 安装管理脚本 ${Red}[${shell_version}]${Font}"
+    echo -e "\t---authored by ly19811105---"
+    echo -e "\thttps://github.com/ly19811105\n"
+    echo -e "当前已安装版本:${shell_mode}\n"
+
+    echo -e "—————————————— 安装向导 ——————————————"""
+    echo -e "${Green}0.${Font}  升级 脚本"
+    echo -e "${Green}1.${Font}  安装 V2Ray (Nginx+ws+tls)"
+    echo -e "${Green}2.${Font}  安装 V2Ray (http/2)"
+    echo -e "${Green}3.${Font}  升级 V2Ray core"
+    echo -e "—————————————— 配置变更 ——————————————"
+    echo -e "${Green}4.${Font}  变更 UUID"
+    echo -e "${Green}5.${Font}  变更 alterid"
+    echo -e "${Green}6.${Font}  变更 port"
+    echo -e "${Green}7.${Font}  变更 TLS 版本(仅ws+tls有效)"
+    echo -e "—————————————— 查看信息 ——————————————"
+    echo -e "${Green}8.${Font}  查看 实时访问日志"
+    echo -e "${Green}9.${Font}  查看 实时错误日志"
+    echo -e "${Green}10.${Font} 查看 V2Ray 配置信息"
+    echo -e "—————————————— 其他选项 ——————————————"
+    echo -e "${Green}11.${Font} 安装 4合1 bbr 锐速安装脚本"
+    echo -e "${Green}12.${Font} 安装 MTproxy(支持TLS混淆)"
+    echo -e "${Green}13.${Font} 证书 有效期更新"
+    echo -e "${Green}14.${Font} 卸载 V2Ray"
+    echo -e "${Green}15.${Font} 更新 证书crontab计划任务"
+    echo -e "${Green}16.${Font} 清空 证书遗留文件"
+    echo -e "${Green}17.${Font} 退出 \n"
+
+    read -rp "请输入数字：" menu_num
+    case $menu_num in
+    0)
+        update_sh
+        ;;
+    1)
+        shell_mode="ws"
+        install_v2ray_ws_tls
+        ;;
+    2)
+        shell_mode="h2"
+        install_v2_h2
+        ;;
+    3)
+        bash <(curl -L -s https://raw.githubusercontent.com/ly19811105/V2Ray_ws-tls_bash_onekey/${github_branch}/v2ray.sh)
+        ;;
+    4)
+        read -rp "请输入UUID:" UUID
+        modify_UUID
+        start_process_systemd
+        ;;
+    5)
+        read -rp "请输入alterID:" alterID
+        modify_alterid
+        start_process_systemd
+        ;;
+    6)
+        read -rp "请输入连接端口:" port
+        if grep -q "ws" $v2ray_qr_config_file; then
+            modify_nginx_port
+        elif grep -q "h2" $v2ray_qr_config_file; then
+            modify_inbound_port
+        fi
+        start_process_systemd
+        ;;
+    7)
+        tls_type
+        ;;
+    8)
+        show_access_log
+        ;;
+    9)
+        show_error_log
+        ;;
+    10)
+        basic_information
+        if [[ $shell_mode == "ws" ]]; then
+            vmess_link_image_choice
+        else
+            vmess_qr_link_image
+        fi
+        show_information
+        ;;
+    11)
+        bbr_boost_sh
+        ;;
+    12)
+        mtproxy_sh
+        ;;
+    13)
+        stop_process_systemd
+        ssl_update_manuel
+        start_process_systemd
+        ;;
+    14)
+        uninstall_all
+        ;;
+    15)
+        acme_cron_update
+        ;;
+    16)
+        delete_tls_key_and_crt
+        ;;
+    17)
+        exit 0
+        ;;
+    *)
+        echo -e "${RedBG}请输入正确的数字${Font}"
+        ;;
+    esac
+}
+
+list "$1"
