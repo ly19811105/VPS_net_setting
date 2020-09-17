326b70ad1c312bb7bd5ae579f0b89d12e5a90b28
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-09-17 20:22:03 +0800

    Update Network_setting.sh

--- Network_setting.sh
+++ Network_setting.sh
@@ -19,45 +19,40 @@ Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 优化系统配置
 optimizing_system(){
-	sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
-	sed -i '/fs.file-max/d' /etc/sysctl.conf
-	sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
-	sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
-	sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
-	sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
-	echo "net.ipv4.tcp_retries2 = 8
-net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.tcp_fastopen = 3
-fs.file-max = 1000000
-fs.inotify.max_user_instances = 8192
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.ip_local_port_range = 1024 65000
-net.ipv4.tcp_max_syn_backlog = 16384
-net.ipv4.tcp_max_tw_buckets = 6000
-net.ipv4.route.gc_timeout = 100
-net.ipv4.tcp_syn_retries = 1
-net.ipv4.tcp_synack_retries = 1
-net.core.somaxconn = 32768
-net.core.netdev_max_backlog = 32768
-net.ipv4.tcp_timestamps = 0
-net.ipv4.tcp_max_orphans = 32768
-# forward ipv4
-net.ipv4.ip_forward = 1">>/etc/sysctl.conf
+	echo 'fs.file-max = 1024000' >> /etc/sysctl.conf
+	echo 'fs.inotify.max_user_instances = 8192' >> /etc/sysctl.conf
+	echo 'net.core.netdev_max_backlog = 262144' >> /etc/sysctl.conf
+	echo 'net.core.rmem_default = 8388608' >> /etc/sysctl.conf
+	echo 'net.core.rmem_max = 67108864' >> /etc/sysctl.conf
+	echo 'net.core.somaxconn = 65535' >> /etc/sysctl.conf
+	echo 'net.core.wmem_default = 8388608' >> /etc/sysctl.conf
+	echo 'net.core.wmem_max = 67108864' >> /etc/sysctl.conf
+	echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.ip_local_port_range = 10240 65000' >> /etc/sysctl.conf
+	echo 'net.ipv4.route.gc_timeout = 100' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_fastopen = 3' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_fin_timeout = 30' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_keepalive_time = 1200' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_max_orphans = 3276800' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_max_syn_backlog = 65536' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_max_tw_buckets = 60000' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_mem = 94500000 915000000 927000000' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_mtu_probing = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_rmem = 4096 87380 67108864' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_sack = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_syn_retries = 2' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_synack_retries = 2' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_syncookies = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_timestamps = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_tw_reuse = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_window_scaling = 1' >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_wmem = 4096 65536 67108864' >> /etc/sysctl.conf
+	echo 'net.netfilter.nf_conntrack_max = 6553500' >> /etc/sysctl.conf
+	echo 'net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60' >> /etc/sysctl.conf
+	echo 'net.netfilter.nf_conntrack_tcp_timeout_established = 3600' >> /etc/sysctl.conf
+	echo 'net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 120' >> /etc/sysctl.conf
+	echo 'net.netfilter.nf_conntrack_tcp_timeout_time_wait = 120' >> /etc/sysctl.conf
+	echo 'net.nf_conntrack_max = 6553500' >> /etc/sysctl.conf
 	sysctl -p
 	echo "*               soft    nofile           1000000
 *               hard    nofile          1000000">/etc/security/limits.conf
